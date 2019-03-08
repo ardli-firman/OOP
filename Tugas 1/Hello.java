acb1b35724770fb0aadac59d3d82ab8d9dce000c
a0i <estungtung59@gmail.com>
2019-03-08 15:47:47 +0700

    OOP

---
+++ Tugas 1/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Nama\t: Ardli Firman Maulana");
+        System.out.println("NIM\t: 17090081");
+        System.out.println("Kelas\t: 4B");
+    }
+}
